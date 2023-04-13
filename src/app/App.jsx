56d6c470d34ef0a1f913de4d9ea9fcd6bcae4b17
Jihoon <jangna96@gmail.com>
2023-04-13 22:53:15 +0900

    feat: 로그인 정보 확인후 페이지 이동
    - 로그인 되어있을시 홈으로 이동
    - 로그인 되어있지 않을시 로그인 페이지로 이동

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,13 +1,14 @@
 import { Routes, Route, HashRouter as Router } from 'react-router-dom';
-import { Home, Login, Signup } from '@/pages';
+import { First, Home, Login, Signup } from '@/pages';
 import app from '@/@service/app';
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Home />}></Route>
+        <Route path="/" element={<First />}></Route>
         <Route path="/login" element={<Login />}></Route>
+        <Route path="/home" element={<Home />}></Route>
         <Route path="/signup" element={<Signup />}></Route>
       </Routes>
     </Router>
