991ad545136c5bf05ac1707f4ce39c92c9fb226b
Jihoon <jangna96@gmail.com>
2023-04-14 00:33:10 +0900

    feat: 로그인 페이지 완성
    - 스타일링 추가
    - input 및 button 컴포넌트로 교체

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,6 +1,11 @@
+import styled from 'styled-components/macro';
 import { useRef, useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import { getAuth, signInWithEmailAndPassword } from 'firebase/auth';
+
+import logo from '@/assets/icons/logo.svg';
+import { SignInput } from '@/components/Input/SignInput';
+import { SignSubmitButton } from '@/components/Button/SignSubmitButton';
 
 export const Login = () => {
   const initialFormState = {
@@ -38,18 +43,93 @@ export const Login = () => {
   };
 
   return (
-    <div>
-      <h2>Login</h2>
-      <form>
-        <label htmlFor="userEmail">Email</label>
-        <input type="email" id="userEmail" name={'email'} onChange={handleChangeInput} />
-        <br />
-        <label htmlFor="userPassword">Password</label>
-        <input type="password" id="userPassword" name={'password'} onChange={handleChangeInput} />
-        <br />
-        <button onClick={handleSignIn}>로그인</button>
-      </form>
-      <Link to="/signup">회원가입 하기</Link>
-    </div>
+    <StyledLoginContainer>
+      <LoginLogoContainer>
+        <img src={logo} alt="Jistagram" />
+      </LoginLogoContainer>
+      <h2 className="a11y-hidden">Login</h2>
+      <Loginform>
+        <SignInput
+          type={'email'}
+          htmlFor={'userEmail'}
+          name={'email'}
+          placeholder={'이메일을 입력하세요.'}
+          onChange={handleChangeInput}
+        />
+        <SignInput
+          type={'password'}
+          htmlFor={'userPassword'}
+          name={'password'}
+          placeholder={'비밀번호를 입력하세요.'}
+          onChange={handleChangeInput}
+        />
+        <SignSubmitButton type={'button'} onClick={handleSignIn}>
+          로그인
+        </SignSubmitButton>
+      </Loginform>
+      <StyledMoveSignUp>
+        <Link to="/signup">회원가입 하기</Link>
+      </StyledMoveSignUp>
+    </StyledLoginContainer>
   );
 };
+
+const StyledLoginContainer = styled.div`
+  width: 100%;
+  max-width: 25rem;
+  margin: 0 auto;
+`;
+
+const LoginLogoContainer = styled.h1`
+  text-align: center;
+  margin-top: 6.25rem;
+  img {
+    width: 18.75rem;
+  }
+`;
+
+const Loginform = styled.form`
+  width: 100%;
+  display: flex;
+  flex-flow: column nowrap;
+  margin-top: 2.5rem;
+  label {
+    display: block;
+    width: 100%;
+  }
+  input {
+    width: 100%;
+    height: 3.5rem;
+    padding: 0 1.5rem;
+    background-color: #191919;
+    border: 0.0625rem solid #444444;
+    border-radius: 0.625rem;
+    font-size: 1rem;
+    color: #ffffff;
+  }
+  input:-webkit-autofill {
+    -webkit-box-shadow: 0 0 0 1000px #191919 inset;
+    -webkit-text-fill-color: #ffffff;
+  }
+  label + label {
+    margin-top: 1.25rem;
+  }
+  label + button {
+    margin-top: 1.25rem;
+  }
+`;
+
+const StyledMoveSignUp = styled.div`
+  margin-top: 2.5rem;
+  text-align: center;
+  a {
+    display: inline-block;
+    height: 1.5rem;
+    line-height: 1.5rem;
+    padding: 0 2rem;
+    color: #aaaaaa;
+  }
+  a:hover {
+    color: #ffffff;
+  }
+`;
