b52197970222f5c482532bcb6916babc74a3ef01
Jihoon <jangna96@gmail.com>
2023-04-13 21:58:42 +0900

    feat: Header 로그아웃 버튼 추가

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -8,43 +8,82 @@ import {
   faPaperPlane,
   faCircleUser,
 } from '@fortawesome/free-regular-svg-icons';
-import { faHouse, faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
+import {
+  faHouse,
+  faMagnifyingGlass,
+  faArrowRightFromBracket,
+} from '@fortawesome/free-solid-svg-icons';
 import Logo from '@/assets/icons/logo.svg';
+import { PostDetail } from '@/components/PostDetail/PostDetail';
+import { PostDetailPortal } from '@/components/PostDetail/PostDetailPortal';
+import { useState } from 'react';
+import { useBodyScrollLock } from '@/hooks/useBodyScrollLock';
 
-library.add(faCompass, faSquarePlus, faPaperPlane, faCircleUser, faHouse, faMagnifyingGlass);
+library.add(
+  faCompass,
+  faSquarePlus,
+  faPaperPlane,
+  faCircleUser,
+  faHouse,
+  faMagnifyingGlass,
+  faArrowRightFromBracket
+);
 
 export const Header = () => {
+  // const [postDetailOpen, setPostDetailOpen] = useState(false);
+  // const { lockScroll, openScroll } = useBodyScrollLock();
+  // const HandlerPostDetailOpen = () => {
+  //   lockScroll();
+  //   setPostDetailOpen(true);
+  // };
   const navData = [
-    { id: 1, icon: faHouse, link: '/', title: '홈' },
-    { id: 2, icon: faMagnifyingGlass, link: '/', title: '검색' },
-    { id: 3, icon: faCompass, link: '/', title: '탐색 탭' },
-    { id: 4, icon: faPaperPlane, link: '/', title: '메시지' },
-    { id: 5, icon: faSquarePlus, link: '/', title: '만들기' },
-    { id: 6, icon: faCircleUser, link: '/', title: '프로필' },
+    { id: 1, icon: faHouse, link: '/', title: '홈', onClick: null },
+    { id: 2, icon: faMagnifyingGlass, link: '/', title: '검색', onClick: null },
+    { id: 3, icon: faCompass, link: '/', title: '탐색 탭', onClick: null },
+    { id: 4, icon: faPaperPlane, link: '/', title: '메시지', onClick: null },
+    { id: 5, icon: faSquarePlus, link: '/', title: '만들기', onClick: null },
+    { id: 6, icon: faCircleUser, link: '/', title: '프로필', onClick: null },
   ];
 
   return (
-    <StyledHeader>
-      <h1>
-        <Link to="/">
-          <img src={Logo} alt="Jistagram" height="40px" />
-        </Link>
-      </h1>
-      <StyledNav>
-        <ul>
-          {navData.map((list) => {
-            return (
-              <li key={list.id}>
-                <Link to={list.link}>
-                  <FontAwesomeIcon icon={list.icon} />
-                  <span>{list.title}</span>
-                </Link>
-              </li>
-            );
-          })}
-        </ul>
-      </StyledNav>
-    </StyledHeader>
+    <>
+      <StyledHeader>
+        <h1>
+          <Link to="/">
+            <img src={Logo} alt="Jistagram" height="40px" />
+          </Link>
+        </h1>
+        <StyledNav>
+          <ul>
+            {navData.map((list) => {
+              return (
+                <li key={list.id}>
+                  <button onClick={list.onClick}>
+                    <FontAwesomeIcon icon={list.icon} />
+                    <span>{list.title}</span>
+                  </button>
+                </li>
+              );
+            })}
+          </ul>
+        </StyledNav>
+        <BottomContainer>
+          <LogOutButton>
+            <FontAwesomeIcon icon={faArrowRightFromBracket} />
+            <span>로그아웃</span>
+          </LogOutButton>
+        </BottomContainer>
+      </StyledHeader>
+      {/* {postDetailOpen && (
+        <PostDetailPortal>
+          <PostDetail
+            setPostDetailOpen={setPostDetailOpen}
+            HandlerPostDetailClose
+            openScroll={openScroll}
+          />
+        </PostDetailPortal>
+      )} */}
+    </>
   );
 };
 
@@ -67,16 +106,50 @@ const StyledNav = styled.nav`
   li + li {
     margin-top: 0.625rem;
   }
-  a {
+  button {
+    color: #ffffff;
+    background-color: transparent;
+    border: 0;
     display: flex;
     flex-flow: row nowrap;
     align-items: center;
     width: 100%;
     padding: 0.75rem 1rem;
     border-radius: 1.875rem;
+    font-size: 0.875rem;
+    cursor: pointer;
   }
-  a:hover {
+  button:hover {
     background-color: #333;
+  }
+  svg {
+    width: 1.5rem;
+    height: 1.5rem;
+  }
+  span {
+    margin-left: 1.25rem;
+  }
+`;
+
+const BottomContainer = styled.div`
+  background-color: #191919;
+  padding-bottom: 20px;
+  position: absolute;
+  bottom: 0;
+`;
+
+const LogOutButton = styled.button`
+  display: flex;
+  flex-flow: row nowrap;
+  align-items: center;
+  padding: 0.75rem 1rem;
+  background-color: transparent;
+  border: 0;
+  font-size: 0.875rem;
+  color: #ffffff;
+  cursor: pointer;
+  &:hover {
+    color: #ccc;
   }
   svg {
     width: 1.5rem;
